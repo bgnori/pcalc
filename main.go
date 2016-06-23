d45dda14fa606e486fcdba6d4377e0b0ebc28628
nori <bgnori@gmail.com>
2016-06-23 20:49:37 +0900

    impl input by json

--- main.go
+++ main.go
@@ -1,68 +1,37 @@
 package main
 
 import (
-	"flag"
+	"encoding/json"
 	"fmt"
 	. "github.com/bgnori/npoker"
+	"os"
 	"runtime"
 	"time"
 )
 
+type Request struct {
+	Source     string `json:"source"`
+	Players    []Deck `json:"players"`
+	Board      []Deck `json:"board"`
+	Trials     int    `json:"trials"`
+	Goroutines int    `json:"goroutines"`
+}
+
 func main() {
-	//source: https://twitter.com/PokerStarsJapan/status/725880733093400576
-	/*
-		PlayerOneHole := Deck{
-			NewCard{NINE, DIAMONDS},
-			NewCard{SIX, DIAMONDS},
-		}
 
-		PlayerTwoHole := Deck{
-			NewCard{FIVE, CLUBS},
-			NewCard{FIVE, HEARTS},
-		}
+	var r Request
+	decoder := json.NewDecoder(os.Stdin)
+	decoder.Decode(&r)
 
-		PlayerThreeHole := Deck{
-			NewCard{JACK, CLUBS},
-			NewCard{EIGHT, CLUBS},
-		}
+	fmt.Printf("%+v\n", r)
 
-		board := Deck{
-			NewCard{NINE, CLUBS},
-			NewCard{SEVEN, CLUBS},
-			NewCard{FIVE, DIAMONDS},
-			NewCard{FOUR, DIAMONDS},
-		}
-	*/
-
-	nexp := flag.Int("exp", 100, "the number of experiments")
-	nroutine := flag.Int("rtn", 1, "the number of  go routine to calculate board")
-	flag.Parse()
-
-	PlayerOneHole := Deck{
-		NewCard(JACK, SPADES),
-		NewCard(JACK, CLUBS),
-	}
-
-	PlayerTwoHole := Deck{
-		NewCard(ACE, SPADES),
-		NewCard(ACE, HEARTS),
-	}
-
-	PlayerThreeHole := Deck{
-		NewCard(KING, DIAMONDS),
-		NewCard(ACE, DIAMONDS),
-	}
-	board := Deck{}
-
-	fmt.Printf("exp=%d, worker=%d\n", *nexp, *nroutine)
-	fmt.Printf("NumCPU=%d, GOMAXPROCS=%d\n", runtime.NumCPU(), runtime.GOMAXPROCS(0))
-	if *nexp > 1 {
+	if r.Goroutines > 1 {
 		runtime.GOMAXPROCS(runtime.NumCPU())
 	}
-	fmt.Printf("%v\n", board)
-	calc := NewEqCalc(board, []Deck{PlayerOneHole, PlayerTwoHole, PlayerThreeHole})
 
-	ex := NewRunner(*nexp, *nroutine,
+	calc := NewEqCalc(r.Board, r.Players)
+
+	ex := NewRunner(r.Trials, r.Goroutines,
 		calc,
 		NewEqSummarizer(calc),
 	)
@@ -73,11 +42,4 @@ func main() {
 
 	fmt.Printf("%f秒\n", (end.Sub(start)).Seconds())
 	fmt.Println(ex.Summary())
-
-	/*
-		players := []Deck{PlayerOneHole, PlayerTwoHole, PlayerThreeHole}
-		for i, count := range stat {
-			fmt.Printf("player %d has %s, won %d times.\n", i, players[i], count)
-		}
-	*/
 }
