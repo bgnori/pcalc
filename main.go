13fe28c42ec45c090cdce48727f8556ab958d510
nori <bgnori@gmail.com>
2016-07-26 22:23:12 +0900

    change follow up struct Request updates

--- main.go
+++ main.go
@@ -2,20 +2,19 @@ package main
 
 import (
 	"encoding/json"
-	//"fmt"
+	"flag"
 	. "github.com/bgnori/npoker"
 	"os"
 	//"runtime"
+	"strings"
 	"time"
 )
 
-func main() {
-
+func calc(fin *os.File, fout *os.File, trials int) {
 	var req Request
-	decoder := json.NewDecoder(os.Stdin)
-	decoder.Decode(&req)
 
-	//fmt.Printf("%+v\n", req)
+	decoder := json.NewDecoder(fin)
+	decoder.Decode(&req)
 
 	/*
 		if req.Goroutines > 1 {
@@ -23,20 +22,73 @@ func main() {
 		}
 	*/
 
-	w := NewWorkSet(req.Board, req.Players)
+	if trials > 0 {
+		req.Trials = trials
+	}
+
+	if req.Seed == nil {
+		req.Seed = GetSeedFromRAND()
+	}
 
 	r := NewRand()
-	b := GetSeedFromRAND()
-	summary := NewSummary(req, b)
-	r.SeedFromBytes(b)
+	r.SeedFromBytes(req.Seed)
 
+	summary := NewSummary(req)
 	summary.Start = time.Now()
+
+	w := NewWorkSet(req.Board, req.Players)
 	for i := 0; i < req.Trials; i++ {
 		u := w.Clone()
 		summary.Add(u.Run(r))
 	}
 	summary.End = time.Now()
 
-	encoder := json.NewEncoder(os.Stdout)
+	encoder := json.NewEncoder(fout)
 	encoder.Encode(summary)
+}
+
+func main() {
+	var count int
+	var input string
+	var output string
+	var fin *os.File
+	var fout *os.File
+	var err error
+	var auto bool
+
+	flag.IntVar(&count, "c", 0, "count for experiment")
+	flag.BoolVar(&auto, "a", false, "auto filename based on input, *request.json => *result.json")
+	flag.StringVar(&input, "in", "", "input file. default is STDIN")
+	flag.StringVar(&output, "out", "", "output file. default is STDOUT")
+	flag.Parse()
+
+	if len(input) == 0 {
+		fin = os.Stdin
+		input = "STDIN"
+	} else {
+		fin, err = os.Open(input)
+		if err != nil {
+			panic(err)
+		}
+		defer fin.Close()
+	}
+
+	if auto {
+		output = strings.Replace(input, "request.json", "result.json", 1)
+	}
+
+	if len(output) == 0 {
+		fout = os.Stdout
+	} else {
+		fout, err = os.Create(output)
+		if err != nil {
+			if auto {
+				return
+			} else {
+				panic(err)
+			}
+		}
+		defer fout.Close()
+	}
+	calc(fin, fout, count)
 }
